3ad6e7ba76f6d99987b30c53e6930d8919a7657e
sebastian gerena <sebger31@gmail.com>
2024-10-14 23:18:17 -0500

    Ajustes para que funcione backend

--- src/main/java/com/arboriapp/arboriapp/controler/ArboriApi.java
+++ src/main/java/com/arboriapp/arboriapp/controler/ArboriApi.java
@@ -1,8 +1,26 @@
 package com.arboriapp.arboriapp.controler;
 
-import org.springframework.web.bind.annotation.RestController;
+import jdk.jfr.Name;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
-
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ArboriApi {
+
+    @PostMapping("/value")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<String> getTreeValue(@RequestBody Object data){
+
+        Random rand = new Random();
+        int random1 = rand.nextInt(9);
+        int random2 = rand.nextInt(9);
+
+        int value = (100 * (random1*random2));
+
+        return ResponseEntity.ok("$"+String.valueOf(value));
+    }
 }
